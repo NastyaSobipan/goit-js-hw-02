71f320bd527705ed46e801e974217ebd3f8b9ffc
Nastya <anastasia.sobipan81@gmail.com>
2025-03-10 10:02:40 +0200

    fk

--- js/task-1.js
+++ js/task-1.js
@@ -7,14 +7,8 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   }
 }
 
-makeTransaction(5, 3000, 23000);
-makeTransaction(3, 1000, 15000);
-makeTransaction(10, 5000, 8000);
-makeTransaction(8, 2000, 10000);
-makeTransaction(10, 500, 5000);
-
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+console.log(makeTransaction(5, 3000, 23000)); // Очікуваний результат: "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // Очікуваний результат: "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // Очікуваний результат: "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // Очікуваний результат: "You ordered 8 droids worth 16000 credits!"
+console.log(makeTransaction(10, 500, 5000)); // Очікуваний результат: "You ordered 10 droids worth 5000 credits!"
